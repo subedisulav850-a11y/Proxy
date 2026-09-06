352d8326ccb371b54b059ba35ff070aff2d583bc
subedisulav850-a11y <subedisulav850@gmail.com>
2026-09-06 22:54:41 +0545

    Add ExperimentalMaterial3Api annotation to SulavProxyApp

--- app/src/main/java/com/sulav/proxy/MainActivity.kt
+++ app/src/main/java/com/sulav/proxy/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -31,6 +32,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SulavProxyApp() {
     val context = LocalContext.current
